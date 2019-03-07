fd3e210742a595046634909edf9da49363d7908e
Monika Dominic <dominic.monika@gmail.com>
2019-03-07 20:22:01 +0100

    refactored code for toggle menu button

--- script.js
+++ script.js
@@ -2,31 +2,26 @@ var btn = document.querySelector(".toggleMenuBtn");
 var x = document.querySelector(".navBar");
 var a = document.querySelectorAll("a");
 
-btn.addEventListener("click", function(event){
+btn.addEventListener("click", function (event) {
     event.preventDefault();
-    if (x.className === "navBar" || x.className === "navBar nav-colored") {
-        x.classList.add("responsive");
-    } else {
-        x.classList.remove("responsive");
-    }
+    x.classList.toggle("responsive");
 });
 
 
 a.forEach(element => {
-    if(element.className !== "toggleMenuBtn"){
-    element.addEventListener("click", function(event){
-        if (x.classList.contains("responsive")) {
-            x.classList.remove("responsive");
-        }
-    });
-  }
+    if (element.className !== "toggleMenuBtn") {
+        element.addEventListener("click", function (event) {
+            if (x.classList.contains("responsive")) {
+                x.classList.remove("responsive");
+            }
+        });
+    }
 });
 
 window.addEventListener('scroll', function (e) {
     if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
-            x.classList.add('nav-colored');
-        } else {
-            x.classList.remove('nav-colored');
-        }
-    
+        x.classList.add('nav-colored');
+    } else {
+        x.classList.remove('nav-colored');
+    }
 });
